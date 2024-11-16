4d791c05ac0140cce957d3e0a5fa7076126be7a2
tresyoung <tresyoung@hotmail.com>
2024-11-15 19:51:58 -0600

    Committing Flask changes

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
 import os
 from flask import Flask, render_template
 
-os.system('clear')
+os.system("clear")
 
 app = Flask(__name__)
 
-@app.route('/')
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India",
+        "salary": "Rs. 10,00,000",
+    },
+    {
+        "id": 2,
+        "title": "Front End Engineer",
+        "location": "Remote",
+        "salary": "Rs. 12,00,000",
+    },
+    {
+        "id": 3,
+        "title": "Data Scientist",
+        "location": "Delhi, India",
+        "salary": "Rs. 15,00,000",
+    },
+    {
+        "id": 4,
+        "title": "Backend Engineer",
+        "location": "San Francisco, USA",
+        "salary": "$120,000",
+    },
+]
 
+
+@app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template("home.html", jobs=JOBS)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0")
